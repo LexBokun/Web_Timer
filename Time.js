9041d1a7821c1d597e447735ddfd718c73e864b3
LexBokun <alesabokun7571@gmail.com>
2024-03-20 11:45:49 +0300

    main2 main2

--- Time.js
+++ Time.js
@@ -1,5 +1,5 @@
-function clock() {
-    let now = new Date();
+function clock(){
+    var now = new Date();
     let H = ("0"+now.getHours()).slice(-2);
     let M = ("0"+now.getMinutes()).slice(-2);
     let S = ("0"+now.getSeconds()).slice(-2);
@@ -11,7 +11,3 @@ setInterval(() => {
     let a =  document.querySelector('h1')
     a.innerHTML = clock();
 }, 1000);
-//console.log(clock());
-const windowInnerHeight = document.documentElement.clientHeight
-//console.log();
-// .innerHTML = clock();
